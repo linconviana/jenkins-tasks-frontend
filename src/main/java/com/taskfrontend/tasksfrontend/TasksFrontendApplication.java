f1d02c0e07fe8d5a84a8c32e3940572e6a3d1980
Lincon Santos Viana Barbosa <linconviana@gmail.com>
2022-09-28 12:22:43 -0300

    criação do projeto frontend e criação do arquivo war

---
+++ src/main/java/com/taskfrontend/tasksfrontend/TasksFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.taskfrontend.tasksfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasksFrontendApplication.class, args);
+	}
+
+}
