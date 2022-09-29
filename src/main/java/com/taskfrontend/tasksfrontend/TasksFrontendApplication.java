fd987bfc0056834aa4cf3300386118ecb64e55f1
Lincon Santos Viana Barbosa <linconviana@gmail.com>
2022-09-28 23:35:57 -0300

    add overide no metodo main

--- src/main/java/com/taskfrontend/tasksfrontend/TasksFrontendApplication.java
+++ src/main/java/com/taskfrontend/tasksfrontend/TasksFrontendApplication.java
@@ -2,12 +2,19 @@ package com.taskfrontend.tasksfrontend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TasksFrontendApplication {
+public class TasksFrontendApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TasksFrontendApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TasksFrontendApplication.class);
 	}
 
 }
